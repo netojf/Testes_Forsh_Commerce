a676ca5edf3ce2b86516bd2a195a673aee71b0c7
netojf <alyce.cristine.s@gmail.com>
2020-12-28 17:01:32 -0300

    Add project files.

---
+++ Tools/TestTools.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+
+namespace Tools
+{
+	public class TestTools
+	{
+      public static IWebElement WaitUntilElementExists(By elementLocator, IWebDriver Driver, int timeout = 10)
+      {
+         try
+         {
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timeout));
+            return wait.Until(ExpectedConditions.ElementExists(elementLocator));
+         }
+         catch (NoSuchElementException)
+         {
+            Console.WriteLine("Element with locator: '" + elementLocator + "' was not found in current context page.");
+            throw;
+         }
+      }
+   }
+}
